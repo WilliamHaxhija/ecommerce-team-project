1142be7d02a4547b2a60ce92a33eccf39035e6f3
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-13 17:02:49 +0200

    feat: add components to app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
+export default {
+    name : 'AppHeader',
+    name: 'AppMain'
+}
+
+export default {
+    components: {
+      AppHeader,
+      AppMain
+ }
+}
 </script>
 
 <template>
-
+<AppHeader></AppHeader>
+<AppMain></AppMain>
 </template>
 
 <style scoped lang="scss">
