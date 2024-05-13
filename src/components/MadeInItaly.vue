41d64248824f7b3b3cf5c6387559b01f0b6c2955
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-05-13 18:05:37 +0200

    fix: img and style

--- src/components/MadeInItaly.vue
+++ src/components/MadeInItaly.vue
@@ -2,70 +2,52 @@
     <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="row">
+                <div class="row m-2 text-center">
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img1.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 1</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img2.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 2</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img3.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 3</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img1.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 4</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img2.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 5</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img3.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 6</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
@@ -73,70 +55,52 @@
 
             </div>
             <div class="carousel-item">
-                <div class="row">
+                <div class="row m-2 text-center">
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img1.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 7</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img2.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 8</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img3.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 9</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img1.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 10</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img2.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 11</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="card" style="width: 18rem;">
-                            <img src="" class="card-img-top" alt="...">
+                            <img src="../assets/img/madeinitaly/img3.png" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card 12</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
@@ -144,11 +108,11 @@
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon" aria-hidden="false"></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon" aria-hidden="false"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
@@ -162,6 +126,10 @@
 .card {
     width: 100% !important;
     margin: 10px;
+    border: none;
+    img {
+        border-radius: 50%;
+    }
 }
 
 .carousel-inner.row {
