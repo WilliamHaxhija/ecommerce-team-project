1444adea19f17b36b10bfba6de455186a91d54c6
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-05-13 17:20:27 +0200

    fix: image url

--- src/components/MadeInItaly.vue
+++ src/components/MadeInItaly.vue
@@ -3,7 +3,7 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="card" style="width: 18rem;">
-                    <img src="image1.jpg" class="card-img-top" alt="...">
+                    <img src="" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card 1</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
@@ -14,7 +14,7 @@
             </div>
             <div class="carousel-item">
                 <div class="card" style="width: 18rem;">
-                    <img src="image2.jpg" class="card-img-top" alt="...">
+                    <img src="" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card 2</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
@@ -25,7 +25,7 @@
             </div>
             <div class="carousel-item">
                 <div class="card" style="width: 18rem;">
-                    <img src="image3.jpg" class="card-img-top" alt="...">
+                    <img src="" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card 3</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
