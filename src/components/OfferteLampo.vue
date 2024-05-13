8be5257673246197267ac1a9fc03748ed41468ce
iTsGiuse <giuse.amato@icloud.com>
2024-05-13 19:13:56 +0200

    feat: add carousel in 'Offerte Lampo'

--- src/components/OfferteLampo.vue
+++ src/components/OfferteLampo.vue
@@ -17,150 +17,172 @@
                     </div>       
                 </div>
             </div>
-            <div class="row">
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row mb-3 mt-3">
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 15% di sconto</p>
+                                        <h5 class="card-title">Cassa</h5>
+                                        <p class="card-text">200$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/61R1EAqX93L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 10% di sconto</p>
+                                        <h5 class="card-title">Scarpe</h5>
+                                        <p class="card-text">100$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/51tS9d9EF2L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 5% di sconto</p>
+                                        <h5 class="card-title">Telecamera</h5>
+                                        <p class="card-text">50$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 15% di sconto</p>
+                                        <h5 class="card-title">Cassa</h5>
+                                        <p class="card-text">200$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/61R1EAqX93L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 10% di sconto</p>
+                                        <h5 class="card-title">Scarpe</h5>
+                                        <p class="card-text">100$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/51tS9d9EF2L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 5% di sconto</p>
+                                        <h5 class="card-title">Telecamera</h5>
+                                        <p class="card-text">50$</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row mb-3 mt-3">
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 15% di sconto</p>
+                                        <h5 class="card-title">Cassa</h5>
+                                        <p class="card-text">200$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/61R1EAqX93L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 10% di sconto</p>
+                                        <h5 class="card-title">Scarpe</h5>
+                                        <p class="card-text">100$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/51tS9d9EF2L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 5% di sconto</p>
+                                        <h5 class="card-title">Telecamera</h5>
+                                        <p class="card-text">50$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 15% di sconto</p>
+                                        <h5 class="card-title">Cassa</h5>
+                                        <p class="card-text">200$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/61R1EAqX93L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 10% di sconto</p>
+                                        <h5 class="card-title">Scarpe</h5>
+                                        <p class="card-text">100$</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="https://m.media-amazon.com/images/I/51tS9d9EF2L._AC_SL1500_.jpg" class="card-img-top" alt="#">
+                                    <div class="card-body text-center">
+                                        <p class="ms-promotion p-2">Fino al 5% di sconto</p>
+                                        <h5 class="card-title">Telecamera</h5>
+                                        <p class="card-text">50$</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2 mt-3 mb-3">
-                    <div class="card">
-                        <img src="https://m.media-amazon.com/images/I/81Lo6rdF-sL._AC_SL1500_.jpg" class="card-img-top" alt="#">
-                        <div class="card-body">
-                            <p class="ms-promotion p-2">Fino al 15% di sconto</p>
-                            <h5 class="card-title">Cassa</h5>
-                            <p class="card-text">200$</p>
-                        </div>
-                    </div>
-                </div>
-                
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Indietro</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Prossimo</span>
+                </button>
             </div>
+
+
         </div>
     </section>
 </template>
 
 <style scoped lang="scss">
     #offerte-lampo{
-
+        background-color: white;
         .container{
             background-color: white;
+        }
 
-            .row{
-                .col-2{
-                    .ms-promotion{
-                        background-color: #CC0C39;
-                        color: white;
-                        border: 1px solid transparent;
-                        border-radius: 5px;
-                        width: 80%;
-                    }
-                }  
+        .card{
+            border: 1px solid transparent !important;
+
+            .ms-promotion{
+                background-color: #CC0C39;
+                color: white;
+                border: 1px solid transparent;
+                border-radius: 5px;
+                width: 90%;
+                margin-left: 10px;
+
             }
         }
+    
     }
 </style>
