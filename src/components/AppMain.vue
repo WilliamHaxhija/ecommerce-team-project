214f0f4690ba5e6528c232e06ade5112d5910d47
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-13 17:41:57 +0200

    feat: add tag main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,25 @@
 <script>
 
-    import OfferteLampo from './OfferteLampo.vue'
-    import MadeInItaly from './MadeInItaly.vue'
-    import CardSection from './CardSection.vue'
+import OfferteLampo from './OfferteLampo.vue'
+import MadeInItaly from './MadeInItaly.vue'
+import CardSection from './CardSection.vue'
 
-    export default {
-        components: {
-            OfferteLampo,
-            MadeInItaly,
-            CardSection
-        }
+export default {
+    components: {
+        OfferteLampo,
+        MadeInItaly,
+        CardSection
     }
+}
 
 </script>
 
 <template>
-    <OfferteLampo></OfferteLampo>
-    <MadeInItaly></MadeInItaly>
-    <CardSection></CardSection>
+    <main>
+        <OfferteLampo></OfferteLampo>
+        <MadeInItaly></MadeInItaly>
+        <CardSection></CardSection>
+    </main>
 </template>
 
 <style scoped lang="scss"></style>
